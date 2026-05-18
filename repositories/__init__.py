2edd7b5bcb92938cee3c11ecb5289626dafd54a8
Suman Gummalla <sgummalla.work@gmail.com>
2026-05-18 15:50:14 -0700

    fix: remove deleted repositories from __init__.py

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,25 +1,23 @@
 from repositories.skill_repository import SkillRepository
 from repositories.agent_repository import AgentRepository
 from repositories.user_repository import UserRepository
-from repositories.user_skill_repository import UserSkillRepository
-from repositories.user_agent_repository import UserAgentRepository
 from repositories.conversation_repository import ConversationRepository
 from repositories.execution_repository import ExecutionRepository
 from repositories.message_repository import MessageRepository
 from repositories.artifact_repository import ArtifactRepository
 from repositories.usage_repository import UsageRepository
 from repositories.user_llm_models_repository import UserLLMModelsRepository
+from repositories.user_skill_v2_repository import UserSkillV2Repository
 
 __all__ = [
     "SkillRepository",
     "AgentRepository",
     "UserRepository",
-    "UserSkillRepository",
-    "UserAgentRepository",
     "ConversationRepository",
     "ExecutionRepository",
     "MessageRepository",
     "ArtifactRepository",
     "UsageRepository",
     "UserLLMModelsRepository",
+    "UserSkillV2Repository",
 ]
